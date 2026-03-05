5961b2681646fea9478e283c931bb4575e031ca1
samuzin10 <samuelmuller024@gmail.com>
2026-03-05 20:02:46 -0300

    Created Entity Transiction

--- src/main/java/com/fintrack/fintrack_backend/model/Transaction.java
+++ src/main/java/com/fintrack/fintrack_backend/model/Transaction.java
@@ -1,3 +1,71 @@
+package com.fintrack.fintrack_backend.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Transaction {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private Double amount;
+
+    private String category;
+
+    private LocalDate date;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
 }
--- src/main/java/com/fintrack/fintrack_backend/repository/TransactionRepository.java
+++ src/main/java/com/fintrack/fintrack_backend/repository/TransactionRepository.java
@@ -1,3 +1,8 @@
-public class TransactionRepository {
-    
+package com.fintrack.fintrack_backend.repository;
+import com.fintrack.fintrack_backend.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
